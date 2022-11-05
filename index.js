153832d181ac05d96d4c5596cc70adddd3bed1b4
boburbekburkhonov <zafaroviccc@gmail.com>
2022-11-05 19:47:40 +0500

    grupa send

--- index.js
+++ index.js
@@ -13,104 +13,126 @@ const bot = new TelegramBot(process.env.TOKEN, {
   polling: true,
 })
 
-bot.onText(/\/start/, msg => {
-  bot.sendMessage(msg.chat.id, `Salom ${msg.chat.first_name == undefined ? msg.from.first_name : msg.chat.first_name}`, {
-    reply_markup: {
-      keyboard: keyboards.menu,
-      resize_keyboard: true
-    }
-  })
-})
-
-bot.on('message', msg => {
-  if(msg.text == 'Bizning kurslar'){
-    bot.sendMessage(msg.chat.id, 'Bizning kurslar', {
-      reply_markup:{
-        keyboard:keyboards.courses,
-        resize_keyboard: true,
-        one_time_keyboard: true
-      }
-    })
-  }
-
-  if(msg.text == 'Asosiy menyu 🔙'){
-    bot.sendMessage(msg.chat.id, 'Bizning kurslar', {
+bot.onText(/\/start/, (msg) => {
+  console.log(msg);
+  bot.sendMessage(
+    msg.chat.id,
+    `Salom ${
+      msg.chat.first_name == undefined
+        ? msg.from.first_name
+        : msg.chat.first_name
+    }`,
+    {
       reply_markup: {
-        keyboard:keyboards.menu,
-        resize_keyboard: true
-      }
-    })
+        keyboard: keyboards.menu,
+        resize_keyboard: true,
+      },
+    }
+  );
+});
+
+bot.on("message", (msg) => {
+  if (msg.text == "Bizning kurslar") {
+    bot.sendMessage(msg.chat.id, "Bizning kurslar", {
+      reply_markup: {
+        keyboard: keyboards.courses,
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      },
+    });
   }
-})
 
-bot.on('message', msg => {
-  const foundCourse = read('courses.json').find(e => e.name == msg.text)
+  if (msg.text == "Asosiy menyu 🔙") {
+    bot.sendMessage(msg.chat.id, "Bizning kurslar", {
+      reply_markup: {
+        keyboard: keyboards.menu,
+        resize_keyboard: true,
+      },
+    });
+  }
+});
 
-  if(foundCourse){
-    bot.sendPhoto(msg.chat.id, './nodejs.png', {
+bot.on("message", (msg) => {
+  const foundCourse = read("courses.json").find((e) => e.name == msg.text);
+
+  if (foundCourse) {
+    bot.sendPhoto(msg.chat.id, "./nodejs.png", {
       caption: `
         <i>
           ${foundCourse.desc}
         </i>\n<span class='tg-spoiler'>${foundCourse.price}</span>
       `,
-      parse_mode: 'HTML',
+      parse_mode: "HTML",
       reply_markup: {
         inline_keyboard: [
           [
             {
-              text: 'Ro\'yhatdan o\'tish',
-              callback_data:`${foundCourse.name}`
-            }
-          ]
-        ]
-      }
-    })
+              text: "Ro'yhatdan o'tish",
+              callback_data: `${foundCourse.name}`,
+            },
+          ],
+        ],
+      },
+    });
   }
-})
+});
 
-bot.on('callback_query', async msg => {
+bot.on("callback_query", async (msg) => {
   const chatId = msg.message.chat.id;
 
-  if(msg.data) {
-    const userContact = await bot.sendMessage(chatId, "Kontktiangizni kiriting", {
-      reply_markup: JSON.stringify({
-        keyboard: [
-          [
-            {
-              text:'Kontaktingizni kiriting!',
-              request_contact: true
-            }
-          ]
-        ],
-        resize_keyboard: true,
-        force_reply: true,
-        one_time_keyboard: true
-      })
-    })
+  if (msg.data) {
+    const userContact = await bot.sendMessage(
+      chatId,
+      "Kontktiangizni kiriting",
+      {
+        reply_markup: JSON.stringify({
+          keyboard: [
+            [
+              {
+                text: "Kontaktingizni kiriting!",
+                request_contact: true,
+              },
+            ],
+          ],
+          resize_keyboard: true,
+          force_reply: true,
+          one_time_keyboard: true,
+        }),
+      }
+    );
 
-    bot.onReplyToMessage(chatId, userContact.message_id, async contactMsg => {
-      const allRequests = read('requests.json')
+    bot.onReplyToMessage(chatId, userContact.message_id, async (contactMsg) => {
+      const allRequests = read("requests.json");
 
       allRequests.push({
         id: allRequests.at(-1)?.id + 1 || 1,
         course: msg.data,
         address: contactMsg.contact.phone_number,
-        name: contactMsg.from.first_name
-      })
+        name: contactMsg.from.first_name,
+      });
 
-      const newAllRequests = await write('requests.json', allRequests)
+      const newAllRequests = await write("requests.json", allRequests);
 
-      if(newAllRequests){
-        bot.sendMessage(chatId, 'Sizni so\'rovingiz qabul qilindi!', {
-          reply_markup:{
+      if (newAllRequests) {
+        bot.sendMessage(chatId, "Sizni so'rovingiz qabul qilindi!", {
+          reply_markup: {
             keyboard: keyboards.menu,
-            resize_keyboard: true
-          }
-        })
+            resize_keyboard: true,
+          },
+        });
+
+        bot.sendMessage(
+          "-1001890591171",
+          `
+          course: ${msg.data},
+          address: ${contactMsg.contact.phone_number},
+          name: ${contactMsg.from.first_name},
+        `
+        );
       }
-    })
+    });
   }
-})
+});
 
 app.get('/courses', (req, res) => {
   res.json(read('courses.json'))
